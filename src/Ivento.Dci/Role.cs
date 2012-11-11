3e2d9802ed18066b04b53ebbcc06490bae6cea50
Andreas Soderlund <ciscoheat@gmail.com>
2012-11-11 21:31:53 +0100

    Documented Ivento.Dci/Role.cs

--- src/Ivento.Dci/Role.cs
+++ src/Ivento.Dci/Role.cs
@@ -6,16 +6,41 @@ namespace Ivento.Dci
 {
     public static class Role
     {
+        /// <summary>
+        /// Make an object act like a Role.
+        /// </summary>
+        /// <typeparam name="T">Role Contract for the Role.</typeparam>
+        /// <param name="obj">Object that will play the Role.</param>
+        /// <returns>The object, duck typed to the RoleContract type T.</returns>
+        /// <remarks>
+        /// The object returned will have its own type, so to get the original 
+        /// object use Role.IsA[T]
+        /// </remarks>
         public static T ActLike<T>(this object obj) where T : class
         {
             return Impromptu.ActLike<T>(obj);
         }
 
+        /// <summary>
+        /// Make all objects in an IEnumerable act like a Role.
+        /// </summary>
+        /// <typeparam name="T">Role Contract for the Role.</typeparam>
+        /// <param name="objects">Enumerable objects that will play the Role.</param>
+        /// <returns>An IEnumerable with all objects acting implementing the Role Contract.</returns>
         public static IEnumerable<T> AllActLike<T>(this IEnumerable<object> objects) where T : class
         {
             return Impromptu.AllActLike<T>(objects);
         }
 
+        /// <summary>
+        /// Cast a RolePlayer back to its original type.
+        /// </summary>
+        /// <typeparam name="T">Original type for the object.</typeparam>
+        /// <param name="rolePlayer">The Roleplaying object.</param>
+        /// <remarks>
+        /// Because ActLike creates a whole new type, this method is needed for 
+        /// conversion back to the original type.
+        /// </remarks>
         public static T IsA<T>(this object rolePlayer) where T : class
         {
             var output = rolePlayer as IActLikeProxy;
